bf5f8790031fe4ad541f1602a90313d2618b9699
Oliver Louis <59543320+oliverlouis@users.noreply.github.com>
2025-05-11 10:53:34 +0200

    replace divs inside button with span

--- components/task-item/TaskItem.vue
+++ components/task-item/TaskItem.vue
@@ -31,16 +31,16 @@ const handleToggle = (): void => {
           class="mt-1 flex-shrink-0 focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-full"
           :aria-label="task.completed ? 'Mark as pending' : 'Mark as complete'"
       >
-        <div v-if="isLoading"
-             class="w-6 h-6 rounded-full border-2 border-t-transparent border-blue-500 animate-spin"></div>
-        <div v-else-if="task.completed" class="w-6 h-6 rounded-full bg-green-700 flex items-center justify-center">
+        <span v-if="isLoading"
+             class="w-6 h-6 rounded-full border-2 border-t-transparent border-blue-500 animate-spin block"></span>
+        <span v-else-if="task.completed" class="w-6 h-6 rounded-full bg-green-700 flex items-center justify-center block">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 text-white" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd"
                   d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
                   clip-rule="evenodd"/>
           </svg>
-        </div>
-        <div v-else class="w-6 h-6 rounded-full border-2 border-gray-300"></div>
+        </span>
+        <span v-else class="w-6 h-6 rounded-full border-2 border-gray-300 block"></span>
       </button>
       <div>
         <h2 class="text-lg font-semibold leading-tight" :class="[task.completed ? 'line-through text-gray-500' : 'text-gray-800']">
